32d8bc0cd073342468b74c87387e73e1351ec772
Danyil Rozumovskyi <danyil.rozumovskyi@gmail.com>
2025-05-24 23:32:11 +0300

    add generation of a random path from client and expand end point to get data from servers

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -15,12 +17,27 @@ func main() {
 	client := new(http.Client)
 	client.Timeout = 10 * time.Second
 
-	for range time.Tick(1 * time.Second) {
-		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
+	for range time.Tick(time.Second / 10) {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data/%s", *target, generateString()))
 		if err == nil {
-			log.Printf("response %d", resp.StatusCode)
+			data, err := io.ReadAll(resp.Body)
+			if err == nil {
+				log.Printf("response %d: %s", resp.StatusCode, string(data))
+			} else {
+				log.Printf("response %d: read body error (%s)", resp.StatusCode, err)
+			}
 		} else {
 			log.Printf("error %s", err)
 		}
 	}
+}
+
+const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$-_.+!*'()"
+
+func generateString() string {
+	generated := make([]byte, 0, 8)
+	for range 8 {
+		generated = append(generated, chars[rand.Intn(len(chars))])
+	}
+	return string(generated)
 }
